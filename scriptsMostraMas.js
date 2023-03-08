3b70a48153f7aa032c6bae8e23aa74ec8046e9ff
jclaudioeb <jclaudioeb@gmail.com>
2023-03-08 00:06:52 -0300

    inicio
    pagina de principal html

---
+++ scriptsMostraMas.js
@@ -0,0 +1,16 @@
+let hideText_btn = document.getElementById('hideText_btn');
+
+let hideText = document.getElementById('hideText');
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText(){
+    hideText.classList.toggle('show');
+
+    if(hideText.classList.contains('show')){
+        hideText_btn.innerHTML='Ver Menos';
+    }
+    else{
+        hideText_btn.innerHTML='Ver Mas';
+    }
+}
